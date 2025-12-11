708f46be453462d395a486d1577d10b06b1929b1
Your Name <jahedulislamapurbo@gmail.com>
2025-12-11 18:51:21 +0600

    footer section done

--- src/Components/Heading/Heading.jsx
+++ src/Components/Heading/Heading.jsx
@@ -6,7 +6,20 @@ const Heading = () => {
              <h2 className='text-[#0E2954] text-3xl font-semibold mb-5'>Active Auctions</h2>
             <p className='text-black text-xl font-normal mb-8'>Discover and bid on extraordinary items</p>
 
+            <div className="card  bg-white text-black mr-32 w-[42%]">
+  <div className="card-body grid grid-flow-col">
+    <h4 className="card-title">Items</h4>
+    <div className='flex flex-row'>
+        <p>Current Bid</p>
+    <p>Time Left</p>
+    <p>Bid Now</p>
+    </div>
+    
+  </div>
+</div>
+
         </div>
+        
     );
 };
 
